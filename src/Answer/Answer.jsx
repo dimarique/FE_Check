a3564228d1cd73fba66d8101fa7aca9b6a11a593
dimarique <pochtagreen@gmail.com>
2025-09-12 18:08:42 +0200

    everything works but realy ugly

--- src/Answer/Answer.jsx
+++ src/Answer/Answer.jsx
@@ -2,8 +2,7 @@ import Button from "../Button/Button";
 import styles from "./Answer.module.css";
 import { useState } from "react";
 
-const Answer = ({ text }) => {
-  const [isVisible, setIsVisible] = useState(false);
+const Answer = ({ text, isVisible, setIsVisible }) => {
   return (
     <div className={styles.answer_wrapper}>
       <p className={`${styles.answer_text} ${isVisible ? "" : styles.hidden}`}>
