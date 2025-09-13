ea2bc9b82031b55d57ad7d50f9108d033ee071d6
dimarique <pochtagreen@gmail.com>
2025-09-13 10:22:26 +0200

    add id as question number

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,12 @@ import data from "./data/fe_questions_complete.json";
 import Question from "./Question/Question";
 import Answer from "./Answer/Answer";
 import Button from "./Button/Button";
+import Id from "./Id/Id";
 
 function App() {
-  const [index, setIndex] = useState(1);
+  const [index, setIndex] = useState(() =>
+    Math.floor(Math.random() * data.length),
+  );
   const [isVisible, setIsVisible] = useState(false);
 
   const handleButtonClick = () => {
@@ -16,6 +19,7 @@ function App() {
 
   return (
     <>
+      <Id text={data[index].id} />
       <Question text={data[index].question} />
       <Answer
         setIsVisible={setIsVisible}
