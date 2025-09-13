46cf32c45500f5b3e2481f634ee37fadd490aeb7
dimarique <pochtagreen@gmail.com>
2025-09-13 20:57:03 +0200

    add colorcoded tags background, other improvements

--- src/components/Question/Question.jsx
+++ src/components/Question/Question.jsx
@@ -1,10 +1,11 @@
 import styles from "./Question.module.css";
 
 const Question = ({ text, tag }) => {
+  const bageColor = { easy: "green", medium: "yellow", hard: "red" };
   return (
     <div className={styles.question_wrapper}>
       <p className={styles.question_text}>{text}</p>
-      <span className={styles.question_tag}>{tag}</span>
+      <span className={`${styles.question_tag} ${styles[tag]}`}>{tag}</span>
     </div>
   );
 };
