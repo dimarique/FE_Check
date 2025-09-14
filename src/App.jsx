fde50e7e7154ee5b52fcd040f4d455f92f3bfcd2
dimarique <pochtagreen@gmail.com>
2025-09-14 15:33:54 +0200

    add third data part

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
 import "./App.css";
 import { useState } from "react";
-import data1 from "./data/fe_questions_15_30.json";
-import data2 from "./data/fe_questions_30_43.json";
+import data1 from "./data/fe_questions_1_15.json";
+import data2 from "./data/fe_questions_15_30.json";
+import data3 from "./data/fe_questions_30_43.json";
 import Question from "./components/Question/Question";
 import Answer from "./components/Answer/Answer";
 import Button from "./components/Button/Button";
 import Id from "./components/Id/Id";
 
 function App() {
-  const allData = [...data1, ...data2];
+  const allData = [...data1, ...data2, ...data3];
   const randomCard = () => Math.floor(Math.random() * allData.length);
   const [index, setIndex] = useState(() => randomCard());
   const [isVisible, setIsVisible] = useState(false);
   const cardNumber = allData[index];
-
+  console.log(allData);
   const handleButtonClick = () => {
     setIndex(randomCard());
     setIsVisible(false);
